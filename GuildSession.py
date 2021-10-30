0c6ac144f64b82a523cab7fd0f2e6f884213476d
Anubhav Kushwaha <k04kushwaha@gmail.com>
2021-10-30 17:08:27 +0530

    maybe fix e

--- GuildSession.py
+++ GuildSession.py
@@ -4,7 +4,7 @@ import discord.ext.commands
 from discord import VoiceClient
 from discord_slash import SlashContext
 
-from dataFunc import search, get_title
+from dataFunc import get_title
 from utils import start_playing_song, run_async
 
 
@@ -18,10 +18,10 @@ class GuildSession:
         self.current_song = None
         self.bot = bot
         self._stop = False
-    
+
     def queue_empty(self):
         return len(self._queue) == 0
-    
+
     def add_to_queue(self, song: str, ctx: SlashContext):
         try:
             title = get_title(song)
@@ -34,18 +34,18 @@ class GuildSession:
         except IndexError as e:
             print(e)
             run_async(ctx.reply(f"No Results Found"), client=self.bot)
-    
+
     def stop(self):
         self._stop = True
         self.voice_client.stop()
-    
+
     def start_playing(self, force=False):
         if not self.voice_client.is_playing():
             if self._stop:
                 self.current_song = None
                 self._stop = False
                 return
-            
+
             if self.voice_client.is_paused() and not force:
                 self.voice_client.resume()
                 return True
@@ -60,18 +60,18 @@ class GuildSession:
             run_async(self.bot.get_channel(self.channel_id).send(f"Now playing: {video_link}"), self.bot)
             return True
         return False
-    
+
     def skip_song(self, ctx: SlashContext):
         self.voice_client.stop()
         run_async(ctx.reply("Song skipped"), self.bot)
-    
+
     def callback(self, error, *args):
         if error:
             print(error, "please help me")
             self.bot.get_channel(self.channel_id).send(f"Error: {error}")
         else:
             self.start_playing()
-    
+
     def get_titles(self):
         return [x[1] for x in self._queue]
 
@@ -80,24 +80,19 @@ class GuildSessionManager:
     def __init__(self, bot):
         self.sessions = {}
         self._bot = bot
-    
-    def create_session(self, guild_id, voice_client, channel_id, queue: List[str], overwrite=False):
+
+    def create_session(self, guild_id, voice_client, channel_id, queue: List[Tuple[str, str]], overwrite=False) -> (
+            bool, GuildSession):
         if guild_id in self.sessions and not overwrite:
             self.sessions[guild_id].voice_client = voice_client
             self.sessions[guild_id].channel_id = channel_id
-            return False
-        self.sessions[guild_id] = GuildSession(guild_id, voice_client, channel_id, self._bot, queue)
-        return True
-    
+            return False, self.sessions[guild_id]
+        session = self.sessions[guild_id] = GuildSession(guild_id, voice_client, channel_id, self._bot, queue)
+        return True, session
+
     def get_session(self, guild_id) -> GuildSession:
         return self.sessions[guild_id]
-    
-    def get_or_create_session(self, guild_id, voice_client, channel_id, queue: List[str]) -> GuildSession:
-        if guild_id in self.sessions:
-            return self.sessions[guild_id]
-        else:
-            self.create_session(guild_id, voice_client, channel_id, queue)
-            return self.sessions[guild_id]
-    
+
     def remove_session(self, guild_id):
-        del self.sessions[guild_id]
+        if guild_id in self.sessions:
+            del self.sessions[guild_id]

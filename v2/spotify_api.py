dfa168f05897a5220245fc6f45434091ead5b686
Phoenix <prodotzip@gmail.com>
2022-03-12 03:53:11 +0530

    removed debug

--- v2/spotify_api.py
+++ v2/spotify_api.py
@@ -70,7 +70,7 @@ def get_song_spotify(query):
 
     response = requests.get(f'https://api.spotify.com/v1/search?', params=args, headers=headers)
     response_json = response.json()
-    print(response_json)
+
     song = response_json['tracks']['items'][0]
     song = Spotify_Song(
         name=song['name'],
